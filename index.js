6f63df09f17e99defda95a344e11c4c4bb0a0253
Arad Afraz <threewestrick@gmail.com>
2023-12-01 22:45:36 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const words = document.querySelectorAll(".fancy")
+
+words.forEach(word => {
+    const letters = word.innerText.split("")
+    word.innerText = null
+    letters.forEach(letter => {
+        const span = document.createElement("span")
+        span.classList.add('letter')
+        span.innerText = letter
+        span.style.transform = 
+        word.appendChild(span)
+    })
+
+    const chars = word.querySelectorAll(".letter")
+
+    word.addEventListener('mouseenter', event => {
+        chars.forEach(letter => {
+            const top = Math.floor(Math.random() * 80) - 40
+            const left = Math.floor(Math.random() * 60) - 30
+            const deg = Math.floor(Math.random() * 10) - 5
+            letter.style.transform = `translate(${left}%, ${top}%) rotate(${deg}deg)`
+        })
+    })
+
+    word.addEventListener('mouseleave', event => {
+        chars.forEach(letter => {
+            letter.style.transform = null
+        })
+    })
+
+
+
+})
+
